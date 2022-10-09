503241a5d21f6ff713b5f7fe6b0616356a9e61e3
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-10-09 17:30:21 +0200

    bonus milestone 2

--- js/script.js
+++ js/script.js
@@ -8,46 +8,101 @@
 // MILESTONE 1:
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
 
+// MILESTONE 2 (BONUS):
+// Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+// Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
+
+
 // user input
 let pricePerKm = 0.21;
+
+// hidden ticket
+let ticketHidden = document.getElementById("output-data");
+// dropdown userAge values
+// const minorValue = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+// console.log( typeof(minorValue), Array.isArray(minorValue))
+
 // form
 // kilometers to cover
 let kiloMetersInput = document.getElementById("kilometres");
 
 // user age
-let userAgeInput = parseInt((document.getElementById("user-age")).value);
-console.log(userAgeInput)
-// add Event
+let userAgeInput = document.getElementById("user-age");
+
+// name
+let userNameInput = document.getElementById("user-name");
+
 // btn
-// let buttonSubmit = document.getElementById("cta");
-// buttonSubmit.addEventListener("click", function(){
+let buttonSubmit = document.getElementById("cta");
+let buttonCancel = document.getElementById("cancel-btn");
 
-//     let kiloMeters = parseInt(kiloMetersInput.value);
-//     let userAge = parseInt(userAgeInput.value);
+// wagon train randomaizer
+let trainWagon = Math.floor(Math.random() * 10 ) + 1;
+    
+// cp value
+let cpCode = Math.floor(Math.random() * 100000) +1 ;
 
-//     let ticketPrice = kiloMeters * pricePerKm;
-//     console.log(ticketPrice)
-//     // discounts
-//     let childDiscount  = ticketPrice * 0.2;
-//     let oldDiscount  = ticketPrice * 0.4;
+// add listener
+buttonSubmit.addEventListener("click", function(){
 
-//     //child price senior price
-//     const ticketChildPrice = ticketPrice - childDiscount;
-//     const ticketOldPrice = ticketPrice - oldDiscount;
+    let kiloMeters = parseInt(kiloMetersInput.value);
+    // let userAge = parseInt(userAgeInput.value);
+    let userAgeMinor = userAgeInput.options[1];
+    console.log(userAgeMinor)
+    let userAgeAdult = userAgeInput.options[2];
+    console.log(userAgeAdult)
+    let userAgeSenior = userAgeInput.options[3];
+    console.log(userAgeSenior)
+    let userName = userNameInput.value;
+    let price = ""
 
-//     // data elaboration
-//     if(userAge <= 18){
-//         ticketPrice = ticketChildPrice.toFixed(2);
-//     }
-//     else if(userAge >= 65){
-//         ticketPrice = ticketOldPrice.toFixed(2);
-//     }
-//     else{
-//     ticketPrice = ticketPrice.toFixed(2);
-//     }
-//     // reset
-//     userAgeInput.value = ""
-//     kiloMetersInput.value = ""
-//     // output
-//     document.getElementById("ticket-price").innerHTML = "Il prezzo del ticket é:" + ticketPrice + "$";
-// })
+
+    let ticketPrice = kiloMeters * pricePerKm;
+    console.log(ticketPrice)
+    // discounts
+    let childDiscount  = ticketPrice * 0.2;
+    let oldDiscount  = ticketPrice * 0.4;
+
+    //child price senior price
+    const ticketChildPrice = ticketPrice - childDiscount;
+    const ticketOldPrice = ticketPrice - oldDiscount;
+
+    // data elaboration
+    // if (userAge <= 18){
+    if(userAgeMinor.selected == true){
+        ticketPrice = ticketChildPrice.toFixed(2);
+        price = "Biglietto ridotto";
+    }
+    // if (userAge >= 65){
+    else if(userAgeSenior.selected == true){
+        ticketPrice = ticketOldPrice.toFixed(2);
+        price = "Biglietto ridotto";
+    }
+    else if(userAgeAdult.selected == true){
+    ticketPrice = ticketPrice.toFixed(2);
+        price = "Biglietto Standard"
+    }
+
+
+    
+
+    // visible ticket
+    let visibleTicket = ticketHidden.classList.remove("d-none");
+    // output
+
+    document.getElementById("ticket-price").innerHTML = ticketPrice;
+    document.getElementById("cp-code").innerHTML = cpCode;
+    document.getElementById("train-wagon").innerHTML = trainWagon;
+    document.getElementById("offerta").innerHTML = price;
+    document.getElementById("passenger-name").innerHTML = userName;
+})
+
+// cancel listenr
+if()
+buttonCancel.addEventListener("click", function(){
+    kiloMetersInput.value = "";
+    userNameInput.value = "";
+    userAgeInput.value = "";
+    ticketHidden.classList.add("d-none");
+
+})
